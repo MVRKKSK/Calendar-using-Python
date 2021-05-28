d6bd5e760d82c13724b95e043a7b7c6d4a256bcb
MVRKKSK <82573421+MVRKKSK@users.noreply.github.com>
2021-05-28 15:53:34 +0530

    Add files via upload

---
+++ 2021calender.py
@@ -0,0 +1,230 @@
+namee = int(input("enter your desired month number: "))
+count = 7
+if namee == 1:
+    print("january")
+    name = int(input("enter the date "))
+    if name>31:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("friday")
+    if name%count == 2:
+        print("saturday")
+    if name%count == 3:
+        print("sunday")
+    if name%count == 4:
+        print("monday")
+    if name%count == 5:
+        print("tuesday")
+    if name%count == 6:
+        print("wednesday")
+    if name%count == 0:
+        print("thursday")
+if namee == 2:
+    print("february")
+    name = int(input("enter the date "))
+    if name>28:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("monday")
+    if name%count == 2:
+        print("tuesday")
+    if name%count == 3:
+        print("wednesday")
+    if name%count == 4:
+        print("thursday")
+    if name%count == 5:
+        print("friday")
+    if name%count == 6:
+        print("saturday")
+    if name%count == 0:
+        print("sunday")
+if namee == 3:
+    print("march")
+    name = int(input("enter the date "))
+    if name>31:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("monday")
+    if name%count == 2:
+        print("tuesday")
+    if name%count == 3:
+        print("wednesday")
+    if name%count == 4:
+        print("thursday")
+    if name%count == 5:
+        print("friday")
+    if name%count == 6:
+        print("saturday")
+    if name%count == 0:
+        print("sunday")
+if namee == 4:
+    print("april")
+    name = int(input("enter the date "))
+    if name>30:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("thursday")
+    if name%count == 2:
+        print("friday")
+    if name%count == 3:
+        print("saturday")
+    if name%count == 4:
+        print("sunday")
+    if name%count == 5:
+        print("monday")
+    if name%count == 6:
+        print("tuesday")
+    if name%count == 0:
+        print("wednesday")
+if namee == 5:
+    print("may")
+    name = int(input("enter the date "))
+    if name>31:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("saturday")
+    if name%count == 2:
+        print("sunday")
+    if name%count == 3:
+        print("monday")
+    if name%count == 4:
+        print("tuesday")
+    if name%count == 5:
+        print("wednesday")
+    if name%count == 6:
+        print("thursday")
+    if name%count == 0:
+        print("friday")
+if namee == 6:
+    print("june")
+    name = int(input("enter the date "))
+    if name>30:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("tuesday")
+    if name%count == 2:
+        print("wednesday")
+    if name%count == 3:
+        print("thursday")
+    if name%count == 4:
+        print("friday")
+    if name%count == 5:
+        print("saturday")
+    if name%count == 6:
+        print("sunday")
+    if name%count == 0:
+        print("monday")
+if namee == 7:
+    print("july")
+    name = int(input("enter the date "))
+    if name>31:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("thursday")
+    if name%count == 2:
+        print("friday")
+    if name%count == 3:
+        print("saturday")
+    if name%count == 4:
+        print("sunday")
+    if name%count == 5:
+        print("monday")
+    if name%count == 6:
+        print("tuesday")
+    if name%count == 0:
+        print("wednesday")
+if namee == 8:
+    print("august")
+    name = int(input("enter the date "))
+    if name>31:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("sunday")
+    if name%count == 2:
+        print("monday")
+    if name%count == 3:
+        print("tuesday")
+    if name%count == 4:
+        print("wednesday")
+    if name%count == 5:
+        print("thursday")
+    if name%count == 6:
+        print("friday")
+    if name%count == 0:
+        print("saturday")
+if namee == 9:
+    print("september")
+    name = int(input("enter the date "))
+    if name>30:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("wednesday")
+    if name%count == 2:
+        print("thursday")
+    if name%count == 3:
+        print("friday")
+    if name%count == 4:
+        print("saturday")
+    if name%count == 5:
+        print("sunday")
+    if name%count == 6:
+        print("monday")
+    if name%count == 0:
+        print("tuesday")
+if namee == 10:
+    print("october")
+    name = int(input("enter the date "))
+    if name>31:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("friday")
+    if name%count == 2:
+        print("saturday")
+    if name%count == 3:
+        print("sunday")
+    if name%count == 4:
+        print("monday")
+    if name%count == 5:
+        print("tuesday")
+    if name%count == 6:
+        print("wednesday")
+    if name%count == 0:
+        print("thursday")
+if namee == 11:
+    print("november")
+    name = int(input("enter the date "))
+    if name>30:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("monday")
+    if name%count == 2:
+        print("tuesday")
+    if name%count == 3:
+        print("wednesday")
+    if name%count == 4:
+        print("thursday")
+    if name%count == 5:
+        print("friday")
+    if name%count == 6:
+        print("saturday")
+    if name%count == 0:
+        print("sunday")
+if namee == 12:
+    print("december")
+    name = int(input("enter the date "))
+    if name>31:
+        print("the date you entered is wrong")
+    if name%count == 1:
+        print("wednesday")
+    if name%count == 2:
+        print("thursday")
+    if name%count == 3:
+        print("friday")
+    if name%count == 4:
+        print("saturday")
+    if name%count == 5:
+        print("sunday")
+    if name%count == 6:
+        print("monday")
+    if name%count == 0:
+        print("tuesday")
